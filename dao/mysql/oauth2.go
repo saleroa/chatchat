3e7bc99f2a33a3ab534f7429d92b3019129580ea
ViolaPioggia <2604296771@qq.com>
2023-05-14 17:16:27 +0800

    update

---
+++ dao/mysql/oauth2.go
@@ -0,0 +1,18 @@
+package mysql
+
+import (
+	"chatchat/app/global"
+	"fmt"
+	"log"
+)
+
+func AddOauth2User(username, oauth2Username string) (bool, string) {
+	sqlStr := "insert into user_auths(username,oauth2_username) values (?,?)"
+	_, err := global.MysqlDB.Exec(sqlStr, username, oauth2Username)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return false, "another error"
+	}
+	log.Println("insert success")
+	return true, ""
+}
